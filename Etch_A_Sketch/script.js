8b08b130b998b51f20cb64f962b9210aa4f61c6c
Princess <53481093+princessjv@users.noreply.github.com>
2023-04-30 00:17:08 +0800

    replaced select/option with div buttons, reset works, four grid dimention to choose from, just click the button, the grid now generates without overlaping from preiviously generated grid. Start to draw now works.

--- Etch_A_Sketch/script.js
+++ Etch_A_Sketch/script.js
@@ -1,79 +1,90 @@
 //constants
-let outerDivContainer ;
-let innerDivContainer;
-let selectTag;
-//buttons at the top of the screen
-let labels = ['Reset','New_Sketch', 'Dimention','Start_Game'];
-for(let label of labels){//labeling the buttons
-    const buttons = document.createElement('button');
-    document.body.appendChild(buttons);
-    buttons.textContent = label;
-    buttons.setAttribute('id',label);
-    buttons.classList.add('button');/*button design*/ };
+let parentDivContainer;
+let childDivContainer;
+let button;
+let num = 0;
 
-//Grid-dimention select tag
-selectTag = document.createElement('select');
-    let dimentionSizes = [" ","single","double","triple","quadruple"];
-    for(let size in dimentionSizes){
-    const dimention = document.createElement('option');
-    selectTag.appendChild(dimention);
-    dimention.setAttribute('value', dimentionSizes[size]);
-    if(dimention.value == dimentionSizes[0]){dimention.textContent="Canvas Size"}
-    else{dimention.textContent = dimentionSizes[size];}
-
-
-    selectTag.addEventListener('change',()=>{//event starts here
-    if(dimention.value ===dimentionSizes[1]){ let num = 16;}
-    else if(dimention.value ===dimentionSizes[2]){num = 32;}
-    else if(dimention.value ===dimentionSizes[3]){num = 48;}
-    else if(dimention.value ===dimentionSizes[4]){num = 64;}
-    else{num = 80;}//should remove childnode
-
-    for(let counter = 0; counter <num; counter++ ){//Parent
-        outerDivContainer = document.createElement('div');
-            for (let count = 0; count < num; count++) { //child 
-            innerDivContainer = document.createElement('div');
-            outerDivContainer.appendChild(innerDivContainer);
-             innerDivContainer.classList.add('divSize');
-            };
-            document.body.appendChild(outerDivContainer);
-            outerDivContainer.classList.add('divContainer');
-        };//event listiner ends here
-});
-    };
- document.body.appendChild(selectTag);
- selectTag.classList.add('selectModification');
-
-
-
-
-
-
-
-
-  
-   
-
-
-
-
-
-//clicking the start button will change the divboxes bgcolor when mouse hover over it. 
-function startDrawing(){
-    _divSubBoxes.forEach(element => {
-        element.classList.add('coloredBackgroundDiv');});                        
-};
-
-//will reset everything back to default
-function resetToDefault(){
-
-};
-
-
-
-  
-
-
-function createHeaderPanel(){
-   
-    };
+for (let counter = 0; counter < 1; counter++) {
+  //Parent
+  buttonParentDiv = document.createElement("div");
+  //buttons at the top of the screen
+  let buttonLabel = ["Reset", "16x16", "32x32", "48x48", "64x64", "Start", " "];
+  for (let number = 0; number < buttonLabel.length; number++) {
+    button = document.createElement("div");
+    buttonParentDiv.appendChild(button);
+    button.classList.add("button");
+    button.textContent = buttonLabel[number];
+    button.setAttribute("id", buttonLabel[number]);
+  }
+  document.body.appendChild(buttonParentDiv);
+  buttonParentDiv.classList.add("buttonParentDiv");
+}
+const buttons = Array.from(document.querySelectorAll(".button"));
+buttons.forEach((button) =>
+  button.addEventListener("click", () => {
+    if (button.id == "Reset") {
+      resetCanvas();
+    } else if (button.id == "16x16") {
+      num = 16;
+      resetCanvas();
+      createDivBoxes(num);
+    } else if (button.id == "32x32") {
+      num = 32;
+      resetCanvas();
+      createDivBoxes(num);
+    } else if (button.id == "48x48") {
+      num = 48;
+      resetCanvas();
+      createDivBoxes(num);
+    } else if (button.id == "64x64") {
+      num = 64;
+      resetCanvas();
+      createDivBoxes(num);
+    } else if (button.id == "Start") {
+      startDrawing();
+    }
+  })
+);
+function createDivBoxes(num) {
+  let parentArray = [];
+  for (let counter = 0; counter < num; counter++) {
+    //Parent
+    parentDivContainer = document.createElement("div");
+    for (let count = 0; count < num; count++) {
+      //child
+      childDivContainer = document.createElement("div");
+      parentDivContainer.appendChild(childDivContainer);
+      childDivContainer.classList.add("divSize");
+    } //child
+    parentArray.push(counter);
+    document.body.appendChild(parentDivContainer);
+    parentDivContainer.classList.add("divContainer");
+    parentDivContainer.setAttribute("id", parentArray[counter]);
+  } //Parent
+} //function ends here
+function resetCanvas() {
+  const divs = document.querySelectorAll(".divContainer");
+  divs.forEach((div) => {
+    if (div.hasChildNodes) {
+      div.outerHTML = "";
+    }
+  });
+}
+//clicking the start button will change the divboxes bgcolor when mouse hover over it.
+function startDrawing() {
+  alert("start drawing");
+  const divboxes = document.querySelectorAll(".divSize");
+  console.log(divboxes);
+  divboxes.forEach((boxes) =>
+    boxes.addEventListener("mouseover", () => {
+      boxes.classList.add("coloredBackgroundDiv");
+    })
+  );
+}
+function colorPicker() {
+  divboxes.forEach((boxes) => {
+    boxes.addEventListener("click", () => {
+      //todo
+    });
+  });
+}
