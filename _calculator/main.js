d3c384c37f790bda9cccecad3ddcab2d4af11258
Princess <53481093+princessjv@users.noreply.github.com>
2023-07-09 19:06:20 +0800

    .

--- _calculator/main.js
+++ _calculator/main.js
@@ -27,38 +27,38 @@ BUTTONS.forEach((button) =>{
   });
 });
 
-// function calculate(operator, num1, num2) {
-//   switch (operator) {
-//     case "+":
-//       FUNCTION.innerHTML = "ADDITION";
-//       return num1 + num2;
-//     case "-":
-//       FUNCTION.innerHTML = "SUBTRACTION";
-//       return num1 - num2;
-//     case "*":
-//       FUNCTION.innerHTML = "MULTIPLICATION";
-//       return num1 * num2;
-//     case "/":
-//       FUNCTION.innerHTML = "DIVISION";
-//       if (num2 === 0) {
-//         return "Error: Division by zero";
-//       }
-//       return num1 / num2;
-//     default:
-//       return "Error: Invalid operator";
-//   }
-// }
+function calculate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      FUNCTION.innerHTML = "ADDITION";
+      return num1 + num2;
+    case "-":
+      FUNCTION.innerHTML = "SUBTRACTION";
+      return num1 - num2;
+    case "*":
+      FUNCTION.innerHTML = "MULTIPLICATION";
+      return num1 * num2;
+    case "/":
+      FUNCTION.innerHTML = "DIVISION";
+      if (num2 === 0) {
+        return "Error: Division by zero";
+      }
+      return num1 / num2;
+    default:
+      return "Error: Invalid operator";
+  }
+}
 
-// Function to read user input and perform calculation
-// function performCalculation() {
-//   var operator = prompt("Enter an operator (+, -, *, /):");
-//   var num1 = parseFloat(prompt("Enter the first number:"));
-//   var num2 = parseFloat(prompt("Enter the second number:"));
+//Function to read user input and perform calculation
+function performCalculation() {
+  var operator = prompt("Enter an operator (+, -, *, /):");
+  var num1 = parseFloat(prompt("Enter the first number:"));
+  var num2 = parseFloat(prompt("Enter the second number:"));
 
-//   var result = calculate(operator, num1, num2);
+  var result = calculate(operator, num1, num2);
 
-//   alert("Result: " + result);
-// }
+  alert("Result: " + result);
+}
 
-// Call the function to start the calculator
-//performCalculation();
+//Call the function to start the calculator
+performCalculation();
