463c99d7b155ebd3acfde4e98c277f7c66d205ef
Princess <53481093+princessjv@users.noreply.github.com>
2023-07-09 18:29:22 +0800

    .

--- _calculator/main.js
+++ _calculator/main.js
@@ -3,49 +3,62 @@ const RESULT = document.querySelector(".result");
 const FUNCTION = document.querySelector(".function");
 const EQUALS = document.querySelector(".equals");
 const BUTTONS = document.querySelectorAll('.buttons div');
-
+var reg = /[0-9]/;
 EQUALS.addEventListener('click', ()=>{
   prompt("hello");
 });
 
 BUTTONS.forEach((button) =>{
   button.addEventListener('click', ()=>{
-    
+
+    if (button.textContent == "-"){
+    FUNCTION.innerHTML = "SUBTRACTION";}
+    else if (button.textContent =="*"){
+      FUNCTION.innerHTML = "MULTIPLICATION";}
+    else if(button.textContent =="+"){
+      FUNCTION.innerHTML = "ADDITION";}
+    else if(button.textContent =="/"){
+      FUNCTION.innerHTML = "DIVISION";}
+    else if(button.textContent == reg){
+      
+    }
+
+
   });
 });
 
-function calculate(operator, num1, num2) {
-  switch (operator) {
-    case "+":
-      FUNCTION.innerHTML = "ADDITION";
-      return num1 + num2;
-    case "-":
-      FUNCTION.innerHTML = "SUBTRACTION";
-      return num1 - num2;
-    case "*":
-      FUNCTION.innerHTML = "MULTIPLICATION";
-      return num1 * num2;
-    case "/":
-      FUNCTION.innerHTML = "DIVISION";
-      if (num2 === 0) {
-        return "Error: Division by zero";
-      }
-      return num1 / num2;
-    default:
-      return "Error: Invalid operator";
-  }
-}
+// function calculate(operator, num1, num2) {
+//   switch (operator) {
+//     case "+":
+//       FUNCTION.innerHTML = "ADDITION";
+//       return num1 + num2;
+//     case "-":
+//       FUNCTION.innerHTML = "SUBTRACTION";
+//       return num1 - num2;
+//     case "*":
+//       FUNCTION.innerHTML = "MULTIPLICATION";
+//       return num1 * num2;
+//     case "/":
+//       FUNCTION.innerHTML = "DIVISION";
+//       if (num2 === 0) {
+//         return "Error: Division by zero";
+//       }
+//       return num1 / num2;
+//     default:
+//       return "Error: Invalid operator";
+//   }
+// }
 
 // Function to read user input and perform calculation
-function performCalculation() {
-  var operator = prompt("Enter an operator (+, -, *, /):");
-  var num1 = parseFloat(prompt("Enter the first number:"));
-  var num2 = parseFloat(prompt("Enter the second number:"));
+// function performCalculation() {
+//   var operator = prompt("Enter an operator (+, -, *, /):");
+//   var num1 = parseFloat(prompt("Enter the first number:"));
+//   var num2 = parseFloat(prompt("Enter the second number:"));
 
-  var result = calculate(operator, num1, num2);
+//   var result = calculate(operator, num1, num2);
 
-  alert("Result: " + result);
-}
+//   alert("Result: " + result);
+// }
 
 // Call the function to start the calculator
-performCalculation();
+//performCalculation();
