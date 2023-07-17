6b627cbcea816659127142a35f73b8c1a3a2ca9c
Princess <53481093+princessjv@users.noreply.github.com>
2023-07-17 10:54:54 +0800

    .

--- _calculator/main.js
+++ _calculator/main.js
@@ -143,17 +143,3 @@ function calculate(operator, num1, num2) {
       return "Error: Invalid operator";
   }
 }
-
-//Function to read user input and perform calculation
-function performCalculation() {
-  var operator = prompt("Enter an operator (+, -, *, /):");
-  var num1 = parseFloat(prompt("Enter the first number:"));
-  var num2 = parseFloat(prompt("Enter the second number:"));
-
-  var result = calculate(operator, num1, num2);
-
-  alert("Result: " + result);
-}
-
-//Call the function to start the calculator
-performCalculation();
